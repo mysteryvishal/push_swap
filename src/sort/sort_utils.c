dd90a1a29cd825c4db0619733791ace07003eb84
Vishal Mistry <vishalmistry51@gmail.com>
2026-03-06 09:25:28 +0000

    feat: add find_position function to locate value in stack

--- src/sort/sort_utils.c
+++ src/sort/sort_utils.c
@@ -6,7 +6,7 @@
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 18:33:27 by vmistry           #+#    #+#             */
-/*   Updated: 2026/03/06 08:44:29 by vmistry          ###   ########.fr       */
+/*   Updated: 2026/03/06 09:23:08 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,4 +86,21 @@ int	find_max(t_list *stack)
 		current = current->next;
 	}
 	return (max);
+}
+
+int	find_position(t_list *stack, int value)
+{
+	t_list	*current;
+	int	idx;
+
+	current = stack;
+	idx = 0;
+	while (current && current->next)
+	{
+		if (*(int *)current->content == value)
+			return (idx);
+		idx++;
+		current = current->next;
+	}
+	return (-1);
 }
