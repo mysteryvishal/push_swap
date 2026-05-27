6f25b61992baefaa1af91393ab73748007be0af2
Vishal Mistry <vishalmistry51@gmail.com>
2026-05-27 10:08:42 +0100

    fix: correct return value in get_target_pos function

--- src/sort/sort_utils.c
+++ src/sort/sort_utils.c
@@ -6,7 +6,7 @@
 /*   By: vmistry <vmistry@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 18:33:27 by vmistry           #+#    #+#             */
-/*   Updated: 2026/05/07 20:39:53 by vmistry          ###   ########.fr       */
+/*   Updated: 2026/05/27 10:08:03 by vmistry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,7 @@ int	get_target_pos(t_list *stack, int value)
 		stack = stack->next;
 		pos++;
 	}
-	return (0);
+	return (pos);
 }
 
 void	rotate_to_position(t_list **stack, int pos)
